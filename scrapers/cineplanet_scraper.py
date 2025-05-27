37fff20c286607651a7df7490d35eac8b29c0400
Alvaro Flores <asflum@proton.me>
2025-05-26 20:40:38 -0500

    refactor: Cineplanet scraper

--- scrapers/cineplanet_scraper.py
+++ scrapers/cineplanet_scraper.py
@@ -13,6 +13,7 @@ import json, os
 class CineplanetScraper(BaseScraper):
 
     def accept_cookies(self, page: Page):
+        # Espera y hace clic en el botón "Aceptar Cookies" para cerrar el aviso, si existe
         try:
             button = page.wait_for_selector('button:has-text("Aceptar Cookies")')
             if button and button.inner_text().strip() == "Aceptar Cookies":
@@ -21,6 +22,7 @@ class CineplanetScraper(BaseScraper):
             print("No se encontró botón de cookies o hubo un problema:", e)
 
     def apply_filters(self, page: Page) -> List[str]:
+        # Selecciona todos los filtros y solo aplica los de "Ciudad", "Cine" y "Día"
         filters = page.query_selector_all(".movies-filter--filter-category-accordion")
         city_filter_added = False
         cinema_filter_added = False
@@ -28,21 +30,21 @@ class CineplanetScraper(BaseScraper):
         data: List = []
         for filter in filters:
             if not city_filter_added:
-                # Aplicar filtros de ciudad
+                # Aplica el filtro de "Ciudad"
                 raw_data = self._apply_specific_filter(filter, "Ciudad", page)
                 city, city_filter_added = raw_data
                 if city_filter_added:
                     data.append(city)
                     continue
             if not cinema_filter_added:
-                # Aplicar filtros de cines
+                # Aplica el filtro de "Cine"
                 raw_data = self._apply_specific_filter(filter, "Cine", page)
                 cinema, cinema_filter_added = raw_data
                 if cinema_filter_added:
                     data.append(cinema)
                     continue
             if not day_filter_added:
-                # Aplicar filtros de día
+                # Aplica el filtro de "Día"
                 raw_data = self._apply_specific_filter(filter, "Día", page)
                 day, day_filter_added = raw_data
                 if day_filter_added:
@@ -62,8 +64,8 @@ class CineplanetScraper(BaseScraper):
         if not title_element:
             return ("Missing filter title", False)
         if title_element.inner_text().strip() == specific_filter:
-            # Activar acordeón
             classes = filter.get_attribute("class")
+            # Verificar si el acordeón del filtro está expandido
             if "accordion_expanded" not in classes:
                 filter.click()
 
@@ -71,22 +73,24 @@ class CineplanetScraper(BaseScraper):
                 ".movies-filter--filter-category-list-item-label"
             )
 
-            # Escoger filtro
-            if specific_filter == "Ciudad":
-                city, city_selected = self._select_city(items, page)
-                return (city, city_selected)
-            elif specific_filter == "Cine":
-                cinema, cinema_selected = self._select_cinema(items, page)
-                return (cinema, cinema_selected)
-            elif specific_filter == "Día":
-                day, day_selected = self._select_day(items, page)
-                return (day, day_selected)
+            filters = {
+                "Ciudad": self._select_city,
+                "Cine": self._select_cinema,
+                "Día": self._select_day,
+            }
+
+            if specific_filter in filters:
+                return filters[specific_filter](items, page)
+
+        # Si no hay ningún filtro que coincida, se retorna nada
         return ("Filters don't matches", False)
 
     def _select_city(self, cities: List[ElementHandle], page: Page) -> Tuple[str, bool]:
+        # Pedirle al usuario que ingrese una ciudad
         city_chosen = slugify(input("Escoja una ciudad: ")).strip().lower()
         for city in cities:
             city_text = city.inner_text().strip()
+            # Verificar si la ciudad ingresada por el usuario coincide con las opciones de Cineplanet
             if slugify(city_text) == city_chosen:
                 city.click()
                 page.wait_for_function(
@@ -104,6 +108,7 @@ class CineplanetScraper(BaseScraper):
         length = []
         for cine in cinemas:
             length.append(len(cine.inner_text().strip()))
+        # Imprime la lista de cines que tiene Cineplanet, según la ciudad escogida
         max_length = max(length)
         width_length = max_length + 4
         for i in range(0, len(cinemas), 3):
@@ -114,6 +119,7 @@ class CineplanetScraper(BaseScraper):
                     cine = f"{idx + 1}) {cinemas[idx].inner_text().strip()}"
                     fila += cine.ljust(width_length)
             print(fila)
+        # Se le pide al usuario escoger un cine
         cinema_chosen = int(input("Seleccione el número del cine: ").strip())
         for cine in cinemas:
             if (
@@ -132,6 +138,7 @@ class CineplanetScraper(BaseScraper):
         return ("No match found", False)
 
     def _select_day(self, days: List[ElementHandle], page: Page) -> Tuple[str, bool]:
+        # Se le pide al usuario escoger un día
         day_chosen = slugify(input("Escoja un día: ").strip().lower())
         for day in days:
             day_text = slugify(day.inner_text().strip())
@@ -148,6 +155,7 @@ class CineplanetScraper(BaseScraper):
         return ("No match found", False)
 
     def load_all_movies(self, page: Page):
+        # Si hay botón de "Ver más", se presiona. De lo contrario, se salta la función
         try:
             page.wait_for_selector(".movies-list--view-more-button", timeout=2000)
             while True:
@@ -164,6 +172,7 @@ class CineplanetScraper(BaseScraper):
             return
 
     def scrape_showtimes_data(self, page: Page, movie_data: dict):
+        # Construir el diccionario de los cines y los horarios de proyección de la película
         showtimes_by_cinema: dict = {}
         cinema_elements = page.query_selector_all(".film-detail-showtimes--accordion")
         for cine_idx in range(len(cinema_elements)):
@@ -192,11 +201,13 @@ class CineplanetScraper(BaseScraper):
     def _build_showtime_entry(
         self, page: Page, cine_idx: int, container_idx: int
     ) -> dict:
+        # Formar el diccionario con las claves de formato, lenguaje y horarios de proyección
         cinema_elements = page.query_selector_all(".film-detail-showtimes--accordion")
         cine = cinema_elements[cine_idx]
         containers = cine.query_selector_all(".cinema-showcases--sessions-details")
         container = containers[container_idx]
         formats = container.query_selector(".sessions-details--formats")
+        # Extraer los formatos y lenguaje
         dimension = (
             formats.query_selector(".sessions-details--formats-dimension")
             .inner_text()
@@ -213,17 +224,10 @@ class CineplanetScraper(BaseScraper):
             .strip()
         )
 
-        # Filtrando los horarios no disponibles
         session_items = container.query_selector_all(".sessions-details--session-item")
         showtimes: List[ElementHandle] = []
         for showtime_idx in range(len(session_items)):
-            showtime_and_link = []
-            showtime = session_items[showtime_idx]
-            class_attribute = showtime.get_attribute("class") or ""
-            if "showtime-selector_disable" in class_attribute:
-                continue
-
-            # Agregando los horarios disponibles
+            # Actualizar nodos después de page.go_back()
             cinema_elements = page.query_selector_all(
                 ".film-detail-showtimes--accordion"
             )
@@ -236,20 +240,10 @@ class CineplanetScraper(BaseScraper):
             session_items = container.query_selector_all(
                 ".sessions-details--session-item"
             )
-            showtime = session_items[showtime_idx]
-            showtime_button = showtime.query_selector(".showtime-selector--link")
-            showtime_text = showtime_button.inner_text().strip()
 
-            showtime_url = self._click_extract_then_go_back(
-                page,
-                showtime_button,
-                "**/compra/**/asientos",
-                ".purchase-seating--seat-map",
-                ".film-detail-showtimes--accordion",
-            )
-
-            showtime_and_link.append(showtime_text)
-            showtime_and_link.append(showtime_url)
+            showtime_and_link = self._parse_showtimes(session_items, showtime_idx, page)
+            if showtime_and_link == []:
+                continue
 
             showtimes.append(showtime_and_link)
         return {
@@ -259,6 +253,32 @@ class CineplanetScraper(BaseScraper):
             "showtimes": showtimes,
         }
 
+    def _parse_showtimes(
+        self, session_items: List[ElementHandle], showtime_idx: int, page: Page
+    ) -> List:
+        # Extrae la hora y su enlace de compra
+        showtime_data = []
+        showtime = session_items[showtime_idx]
+        class_attribute = showtime.get_attribute("class") or ""
+        if "showtime-selector_disable" in class_attribute:
+            return []
+
+        showtime = session_items[showtime_idx]
+        showtime_button = showtime.query_selector(".showtime-selector--link")
+        showtime_text = showtime_button.inner_text().strip()
+
+        showtime_url = self._click_extract_then_go_back(
+            page,
+            showtime_button,
+            "**/compra/**/asientos",
+            ".purchase-seating--seat-map",
+            ".film-detail-showtimes--accordion",
+        )
+
+        showtime_data.append(showtime_text)
+        showtime_data.append(showtime_url)
+        return showtime_data
+
     def _click_extract_then_go_back(
         self,
         page: Page,
@@ -267,6 +287,7 @@ class CineplanetScraper(BaseScraper):
         wait_for_selector_new_page: str,
         wait_for_selector_return_page: str,
     ) -> str:
+        # Ingresa a la página de venta y guarda el URL
         from playwright.sync_api import TimeoutError
 
         try:
@@ -367,7 +388,9 @@ class CineplanetScraper(BaseScraper):
                 self.load_all_movies(page)
                 movies = page.query_selector_all(".movies-list--large-item")
 
-            print(f"Todos los horarios de las películas disponibles en {movie_data["cinema"]} para {movie_data["day"]} han sido guardados exitosamente.")
+            print(
+                f"Todos los horarios de las películas disponibles en {movie_data["cinema"]} para {movie_data["day"]} han sido guardados exitosamente."
+            )
             browser.close()
 
 
